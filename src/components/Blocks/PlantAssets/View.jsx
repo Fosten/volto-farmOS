265a8a3791d0dc111c76896f3d9b7095f2e0e663
fosten <fosten@gmail.com>
2023-07-17 22:36:52 -0400

    Call PlantAsset relationships links

--- src/components/Blocks/PlantAssets/View.jsx
+++ src/components/Blocks/PlantAssets/View.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Table } from 'semantic-ui-react';
 import axios from 'axios';
+import farmOS from '@farmOS/farmOS.js';
 
 /**
  * View description block.
  * @module components/manage/Blocks/Description/View
  */
 
-import PropTypes from 'prop-types';
-import farmOS from '@farmOS/farmOS.js';
-
 /**
  * View description block class.
  * @class View
  * @extends Component
  */
+
 const View = (props) => {
     const [response, setState] = useState({});
+    const [response2, setState2] = useState({});
+    const [response3, setState3] = useState({});
+    const [locationURL, setState4] = useState('null');
+    const [planttypeURL, setState5] = useState('null');
     async function useResponse() {
       try {
         const response = await axios.get(
@@ -26,7 +30,31 @@ const View = (props) => {
           }}
         )
         setState(response.data);
-        window.localStorage.setItem(0, JSON.stringify(response));
+
+        for (let count = 0; count < 13; count++) {
+          const locationURL = response.data.data[count].relationships.location.links.related.href;
+          setState4(locationURL)
+          window.localStorage.setItem(`Location${count}`, JSON.stringify(locationURL));
+          const response2 = await axios.get(
+          `${JSON.parse(localStorage.getItem(`Location${count}`))}`,
+          {headers: {
+            'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))['access_token']}`
+          }})
+          setState2(response2.data);
+          window.localStorage.setItem(`LResponse${count}`, JSON.stringify(response2));
+
+          const planttypeURL = response.data.data[count].relationships.plant_type.links.related.href;
+          setState5(planttypeURL)
+          window.localStorage.setItem(`Planttype${count}`, JSON.stringify(planttypeURL));
+          const response3 = await axios.get(
+          `${JSON.parse(localStorage.getItem(`Planttype${count}`))}`,
+          {headers: {
+            'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))['access_token']}`
+          }})
+          setState3(response3.data);
+          window.localStorage.setItem(`PResponse${count}`, JSON.stringify(response3));
+
+      };
       } catch (err) {
         // eslint-disable-next-line no-console
         console.log(err);
@@ -46,7 +74,7 @@ const View = (props) => {
       const username = window.env.RAZZLE_FARMOS_API_USERNAME;
       const password = window.env.RAZZLE_FARMOS_API_PASSWORD;  
       farm.remote.authorize(username, password)
-      .then(useResponse());
+      .then(useResponse())
     }, []);
     return (
         <div className="container">
@@ -57,18 +85,28 @@ const View = (props) => {
                 <Table.Row>
                   <th>Name</th>
                   <th>Status</th>
-                  <th>Location</th>
                   <th>Plant Type</th>
+                  <th>Location1</th>
+                  <th>Location2</th>
+                  <th>Location3</th>
+                  <th>Location4</th>
+                  <th>Location5</th>
                 </Table.Row>
               </Table.Header>
               <Table.Body>
               {response.data?.map((item, i) => {
+                let newObject1 = JSON.parse(localStorage.getItem(`LResponse${i}`));
+                let newObject2 = JSON.parse(localStorage.getItem(`PResponse${i}`));
                 return (
                   <Table.Row key={i}>
                   <td>{item.attributes.name}</td>
                   <td>{item.attributes.status}</td>
-                  <td>{item.relationships.location.data[0]?.id}</td>
-                  <td>{item.relationships.plant_type.data[0]?.id}</td>
+                  {newObject2.data.data?.map((item, i) => {
+                    return <td key={i}>{item.attributes.name}</td>;
+                  })}
+                  {newObject1.data.data?.map((item, i) => {
+                    return <td key={i}>{item.attributes.name}</td>;
+                  })}
                   </Table.Row>
                 );
                 })}
