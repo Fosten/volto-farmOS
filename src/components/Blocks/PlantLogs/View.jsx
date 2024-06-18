1f9815f217a35335a02ff44a02c676fad68caea7
Fosten <fosten@gmail.com>
2024-06-18 15:58:59 -0400

    Export/import customizer and loginSchema

--- src/components/Blocks/PlantLogs/View.jsx
+++ src/components/Blocks/PlantLogs/View.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'semantic-ui-react';
-import farmOS from '@farmOS/farmOS.js';
 import _ from 'lodash';
+import { customizer } from '@Fosten/volto-farmOS/components/Blocks/PlantLogs/customizer';
+import { farm, loginSchema } from '@Fosten/volto-farmOS/components/Blocks/PlantLogs/loginSchema';
 
 /**
  * View description block.
@@ -20,47 +21,8 @@ const View = (props) => {
   const [propertyValues, setState] = useState({});
   const [hasFilter, setFilter] = useState(true);
   const [isAxiosBusy, setAxiosBusy] = useState(true);
-  function customizer(objValue, srcValue) {
-    if (_.isArray(objValue)) {
-      return objValue.concat(srcValue);
-    }
-  }
 
   useEffect(() => {
-    const remoteConfig = {
-      host: window.env.RAZZLE_FARMOS_API_HOST,
-      clientId: window.env.RAZZLE_FARMOS_API_CLIENT_ID,
-      clientSecret: window.env.RAZZLE_FARMOS_API_CLIENT_SECRET,
-      scope: window.env.RAZZLE_FARMOS_API_SCOPE,
-      getToken: () => JSON.parse(localStorage.getItem('token')),
-      setToken: (token) => localStorage.setItem('token', JSON.stringify(token)),
-    };
-    const options = { remote: remoteConfig };
-    const farm = farmOS(options);
-
-    async function LoginSchema() {
-      const APIlogin = () => {
-        const username = window.env.RAZZLE_FARMOS_API_USERNAME;
-        const password = window.env.RAZZLE_FARMOS_API_PASSWORD;
-        return farm.remote.authorize(username, password);
-      };
-      async function setFarmSchema() {
-        // Try the session storage first...
-        let schema = JSON.parse(localStorage.getItem('schema'));
-        if (schema == null) {
-          // Not in session storage, so fetch schema from the farmOS host.
-          await farm.schema.fetch();
-          schema = farm.schema.get();
-          // Cache in the session storage for next time.
-          localStorage.setItem('schema', JSON.stringify(schema));
-        } else {
-          await farm.schema.set(schema);
-        }
-      }
-      await APIlogin();
-      setFarmSchema();
-    }
-
     let y = 0;
     let n = 20;
     var newarray = [];
@@ -145,7 +107,7 @@ const View = (props) => {
       }
     }
     async function myResponse() {
-      await LoginSchema(farm);
+      await loginSchema();
       myResponse2();
     }
     myResponse();
---
+++ src/components/Blocks/PlantLogs/loginSchema.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import farmOS from '@farmOS/farmOS.js';
+
+const remoteConfig = {
+  host: window.env.RAZZLE_FARMOS_API_HOST,
+  clientId: window.env.RAZZLE_FARMOS_API_CLIENT_ID,
+  clientSecret: window.env.RAZZLE_FARMOS_API_CLIENT_SECRET,
+  scope: window.env.RAZZLE_FARMOS_API_SCOPE,
+  getToken: () => JSON.parse(localStorage.getItem('token')),
+  setToken: (token) => localStorage.setItem('token', JSON.stringify(token)),
+};
+const options = { remote: remoteConfig };
+const farm = farmOS(options);
+
+export async function loginSchema(props) {
+  const APIlogin = () => {
+    const username = window.env.RAZZLE_FARMOS_API_USERNAME;
+    const password = window.env.RAZZLE_FARMOS_API_PASSWORD;
+    return farm.remote.authorize(username, password);
+  };
+  async function setFarmSchema() {
+    // Try the session storage first...
+    let schema = JSON.parse(localStorage.getItem('schema'));
+    if (schema == null) {
+      // Not in session storage, so fetch schema from the farmOS host.
+      await farm.schema.fetch();
+      schema = farm.schema.get();
+      // Cache in the session storage for next time.
+      localStorage.setItem('schema', JSON.stringify(schema));
+    } else {
+      await farm.schema.set(schema);
+    }
+  }
+  await APIlogin();
+  setFarmSchema();
+}
+
+loginSchema.propTypes = {
+  properties: PropTypes.objectOf(PropTypes.any),
+};
+
+export { farm };
