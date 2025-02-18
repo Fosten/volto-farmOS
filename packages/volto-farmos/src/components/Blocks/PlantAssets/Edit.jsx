f71fc0b07526caa42c7606dd1b288c27edca4a3f
Fosten <fosten@gmail.com>
2025-02-18 15:08:50 -0500

    Remove PlantAssets volto block

--- packages/volto-farmos/src/components/Blocks/PlantAssets/Edit.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Edit = (props) => {
-  return (
-    <div>
-      I'm the auto-generated Plant Assets block! There is no need to edit me.
-    </div>
-  );
-};
-export default Edit;
--- packages/volto-farmos/src/components/Blocks/customBlocks.js
+++ packages/volto-farmos/src/components/Blocks/customBlocks.js
@@ -1,25 +1,8 @@
-import PlantAssetsView from '../../components/Blocks/PlantAssets/View';
-import PlantAssetsEdit from '../../components/Blocks/PlantAssets/Edit';
 import PlantLogsView from '../../components/Blocks/PlantLogs/View';
 import PlantLogsEdit from '../../components/Blocks/PlantLogs/Edit';
 import icon from '@plone/volto/icons/list-bullet.svg';
 
 const customBlocks = {
-  plantassets: {
-    id: 'plantassets',
-    title: 'Plant Assets',
-    edit: PlantAssetsEdit,
-    view: PlantAssetsView,
-    icon: icon,
-    group: 'text',
-    restricted: false,
-    mostUsed: false,
-    sidebarTab: 1,
-    security: {
-      addPermission: [],
-      view: [],
-    },
-  },
   plantlogs: {
     id: 'plantlogs',
     title: 'Plant Logs',
